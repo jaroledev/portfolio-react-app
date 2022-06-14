9e431a0db6f481ae61676b329ece5e23e7612a17
jaroledev <dawjarole@gmail.com>
2022-06-14 21:01:52 +0200

    Styles and add image, socials links to header.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I´m</h5>
+        <h1>Javi Rosado</h1>
+        <h5 className="text-light">Fullstack Developper</h5>
+        <CTA/>
+        <HeaderSocials/>
+
+        <div className="me">
+          <img src={ME} alt="about me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
